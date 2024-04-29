229ee4eade44347e23d18262e7ac3c6dde0ca896
amr <amrkhaledelsisy@gmail.com>
2024-04-29 04:34:45 +0300

    Header

---
+++ Heder.h
@@ -0,0 +1,11 @@
+#ifndef heder
+#define heder
+#include <stdio.h>
+#include <string.h>
+#include <conio.h>
+#include <stdlib.h>
+#include "User.h"
+#include "admin.h"
+#include "Mangment.h"
+#include <unistd.h>
+#endif
